6fc075305612c50a091a601427c662eb0cd4592a
nomadicvince <vincent.moore@cosmiqcloud.com>
2017-10-23 19:14:48 -0400

    Basic boilerplate files
    Added basic boilerplate files for projects.

---
+++ php/index.php
@@ -0,0 +1,36 @@
+<?php
+  //Inlcudes
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>PHP Boilerplate</title>
+    <meta name="description" content="PHP Boilerplate code for web apps and projects."/>
+    <meta name="keywords" content="">
+    <meta name="author" content="Vincent Moore">
+
+    <!-- CSS -->
+    <style>
+
+    </style>
+    <!-- CSS external link-->
+    <link rel="stylesheet" href="styles.css">
+    <!-- end CSS area-->
+
+    <!-- Scripts -->
+    <script type="text/javascript">
+
+    </script>
+    <!--External script -->
+    <script src="" charset="utf-8"></script>
+    <!-- end Scripts area-->
+
+  </head>
+  <body>
+<?php
+  echo "Hello World";
+?>
+  </body>
+</html>
